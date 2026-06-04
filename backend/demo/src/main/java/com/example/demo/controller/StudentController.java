f3150a74e842f4272e5853c69bfc499b7734c8f4
Aftab812 <aftab838291@gmail.com>
2026-06-04 14:56:37 +0530

    Day 2 oops concept done

---
+++ backend/demo/src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Student; 
+import org.springframework.web.bind.annotation.*;
+
+@RestController // this can handle rest api like get post etc
+@RequestMapping("/students")// this is the base url for all the api in this controller
+@CrossOrigin(origins = "http://localhost:5173/") // this is to allow cross origin requests from the frontend
+public class StudentController{
+    
+    @GetMapping
+    public Student getStudent(){
+        return new Student(1,"John Doe","Computer Science");
+    }
+}
