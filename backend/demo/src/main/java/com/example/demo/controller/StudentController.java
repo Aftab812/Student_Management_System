fc96a3587b8d08f2a8b1d7c0311ed313228208e1
Aftab812 <aftab838291@gmail.com>
2026-06-04 16:08:57 +0530

    Day 2 reavt integartin complete

--- backend/demo/src/main/java/com/example/demo/controller/StudentController.java
+++ backend/demo/src/main/java/com/example/demo/controller/StudentController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/students")// this is the base url for all the api in this controller
 @CrossOrigin(origins = "http://localhost:5173/") // this is to allow cross origin requests from the frontend
 public class StudentController{
-    
+
     @GetMapping
     public Student getStudent(){
-        return new Student(1,"John Doe","Computer Science");
+        return new Student(1,"Aftab","Computer Science");
     }
 }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,43 @@
-function App(){
-  return{
-    <div>
-      <h1>hello</h1>
+import { useState } from "react";
+
+function App() {
+
+  const [student, setStudent] = useState(null);
+
+  const getStudent = async () => {
+
+    const response = await fetch(
+      "http://localhost:9090/students"
+    );
+
+    const data = await response.json();
+
+    setStudent(data);
+  };
+
+  return (
+    <div style={{ padding: "40px" }}>
+
+      <button onClick={getStudent}>
+        Get Student
+      </button>
+
+      {
+        student && (
+          <div>
+
+            <h2>ID : {student.id}</h2>
+
+            <h2>Name : {student.name}</h2>
+
+            <h2>Course : {student.course}</h2>
+
+          </div>
+        )
+      }
+
     </div>
-  }
+  );
 }
+
+export default App;
